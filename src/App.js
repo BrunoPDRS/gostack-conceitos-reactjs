afc12e28e9dc2d5b40f049d233a9513d260b9e18
Bruno Pedrosa <bgpedrosas@gmail.com>
2020-10-12 15:57:38 -0300

    Added create and remove repo funcionalities

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repos, setRepos] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setRepos(response.data);
+    })
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    await api.post('/repositories', {
+      title: "repo",
+      url: "someurl",
+      techs: [],
+    }).then( response => setRepos([...repos, response.data]));
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`).then(response => {
+      let currentRepos = repos.map(repo => repo);
+      const repoIndex = repos.findIndex(repo => repo.id === id);
+
+      currentRepos.splice(repoIndex, 1);
+
+      return setRepos(currentRepos);
+    });
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repos.map(repo => (
+          <li key={repo.id}>
+            {repo.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleRemoveRepository(repo.id)}>
+              Remover
+            </button>
+          </li>
+          )
+        )}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
